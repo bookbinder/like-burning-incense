749cd20598ce29c2d24cd08d06eda981582573be
bookbinder <carlsenr@gmail.com>
2023-05-30 20:11:35 -0400

    update filename_creator for Sundays in OT

--- scripts/file_creator_SunOT.py
+++ scripts/file_creator_SunOT.py
@@ -1,40 +1,39 @@
 """Create and populate files for offices/sundaysOT directory and
 offices/sundaysOT/notes subdirectory. Will need to edit files in the notes
-subdirectory. For now, store these files in the temp subdirectory of
-the scripts directory. Then move them by hand carefully."""
+subdirectory later with antiphon music. For now, store these files in the temp
+subdirectory of this (scripts) directory. Then move them by hand carefully."""
 
 from pathlib import Path
 from filename_creator import filenamesSunOT
 
+
 lyFiles, lytexFiles, ilyFiles = filenamesSunOT()
 
-# # ily files
-ilyTxt = Path('../skel/notes.ily').read_text()
-for f in ilyFiles:
-    Path(f'temp/notes/{f}').write_text(ilyTxt)
-
-
-# # lytex files
-lytexTxt = Path('../skel/sunOT.lytex').read_text()
-for f in lytexFiles:
-    curLytexTxt = lytexTxt.replace('changeme', f"{f.split('-')[0]}0", 2)
-    curLytexTxt = curLytexTxt.replace('changeme', f"{f.split('-')[0]}1")
-    Path(f'temp/{f}').write_text(curLytexTxt)
-
-
 # # ly files
-antCantorTxt = Path('../skel/Ant-Cantor.ily').read_text()
-antOrganTxt = Path('../skel/Ant-Organ.ily').read_text()
-psalmOrganTxt = Path('../skel/Psalm-Organ.ily').read_text()
+antCantorStr = Path('../skel/Ant-Cantor.ily').read_text()
+antOrganStr = Path('../skel/Ant-Organ.ily').read_text()
+psalmOrganStr = Path('../skel/Psalm-Organ.ily').read_text()
 for f in lyFiles:
     name = '-'.join(f.split('-')[:3])
     if "Ant-Cantor" in f:
-        curAntCantorTxt = antCantorTxt.replace('A0-Vespers-1', name)
-        Path(f'temp/{f}').write_text(curAntCantorTxt)
+        curAntCantorStr = antCantorStr.replace('A0-Vespers-1', name)
+        Path(f'temp/{f}').write_text(curAntCantorStr)
     if "Ant-Organ" in f:
-        curAntOrganTxt = antOrganTxt.replace('A0-Vespers-1', name)
-        Path(f'temp/{f}').write_text(curAntOrganTxt)
+        curAntOrganStr = antOrganStr.replace('A0-Vespers-1', name)
+        Path(f'temp/{f}').write_text(curAntOrganStr)
     if "Psalm-Organ" in f:
         name = '-'.join(f.split('-')[:3])
-        curPsalmOrganTxt = psalmOrganTxt.replace('A0-Vespers-1', name)
-        Path(f'temp/{f}').write_text(curPsalmOrganTxt)
+        curPsalmOrganStr = psalmOrganStr.replace('A0-Vespers-1', name)
+        Path(f'temp/{f}').write_text(curPsalmOrganStr)
+
+# # lytex files
+lytexStr = Path('../skel/sunOT.lytex').read_text()
+for f in lytexFiles:
+    curLytexStr = lytexStr.replace('changeme', f"{f.split('-')[0]}0", 2)
+    curLytexStr = curLytexStr.replace('changeme', f"{f.split('-')[0]}1")
+    Path(f'temp/{f}').write_text(curLytexStr)
+
+# # ily files
+ilyStr = Path('../skel/notes.ily').read_text()
+for f in ilyFiles:
+    Path(f'temp/notes/{f}').write_text(ilyStr)
